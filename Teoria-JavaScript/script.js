d181a94d0bb8f45b0dd1b757f00050256f5df1b2
LucasGodoyFP <conta.lucasgodoy@gmail.com>
2026-03-01 23:15:51 -0300

    Adiciona teoria javascript

---
+++ Teoria-JavaScript/script.js
@@ -0,0 +1,26 @@
+console.log(2 + 8);
+console.log(5 + 3);
+console.log(5 * 4);
+console.log(10 / 2);
+console.log(5 + 4 * 2);
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(2 * "abc");
+numero = 2;
+console.log("um texto");
+console.log("testando a \nquebra de linha");
+console.log("espaçamento \t de tab");
+console.log("lucas" + "godoy" + numero);
+
+console.log(`a soma de 2+2 é: ${2 + 2}`);
+
+console.log(`podemos executar qualquer coisa aqui: ${console.log("teste")}`);
+
+console.log(true);
+
+console.log(typeof true);
+console.log(true && true);
+console.log(`se colocar true e true o resultado é: ${true && true} `);
+
+console.log(!3 > 1);
