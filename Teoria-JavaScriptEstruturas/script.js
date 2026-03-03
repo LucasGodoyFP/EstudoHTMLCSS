9c393e5705271cd154dd2014d060a0c2310b379c
LucasGodoyFP <conta.lucasgodoy@gmail.com>
2026-03-02 23:23:14 -0300

    adiciona teoria if, while, do while, for

--- Teoria-JavaScriptEstruturas/script.js
+++ Teoria-JavaScriptEstruturas/script.js
@@ -2,3 +2,42 @@
 // função é função sem objeto
 console.log(Math.max(1, 8, 40));
 console.log(Math.ceil(2.6));
+
+console.error("erro");
+console.warn("aviso");
+
+const q = 10;
+const w = 15;
+
+if (q > 5 && w > 20) {
+  console.log("sao mais altos");
+} else {
+  console.log("os numeros nao sao mais altos");
+}
+
+if (1 > 2) {
+  console.log("teste1");
+} else if (2 > 1) {
+  console.log("teste2");
+} else if (5 < 10) {
+  console.log("agora sim");
+}
+
+const name = "marcos";
+const idade = 30;
+
+if (name === "joao") {
+  console.log("bem vindo joao");
+} else if (name === "matheus" && idade === 30) {
+  console.log("bem vindo matheus, sua idade é 30");
+} else {
+  console.log("vai toma na peida");
+}
+
+for (i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    console.log("número par!");
+    continue;
+  }
+  console.log(i);
+}
