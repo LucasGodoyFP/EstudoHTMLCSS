afc516b3d01ba081eb1a6bc9d4a21e69831d5527
Lucas Godoy <conta.lucasgodoy@gmail.com>
2026-03-11 19:34:17 -0300

    atualiza

--- JavaScript/Teoria-JavaScriptEventos/script.js
+++ JavaScript/Teoria-JavaScriptEventos/script.js
@@ -20,3 +20,34 @@ meuTitulo.addEventListener("click", (event) => {
   console.log(event.pointerType);
   console.log(event.target);
 });
+
+//Propagação - Quando um filho ativa o evento do pai
+const containerBtn = document.querySelector("#btn-container");
+const btnInside = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+  console.log("evento 1 ");
+});
+
+btnInside.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("evento 2");
+});
+
+const google = document.querySelector("a");
+
+//Remover funcoes padrao
+google.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("clicou e nao foi pro google");
+});
+
+//Eventos de tecla
+
+document.addEventListener("keyup", (event) => {
+  console.log(`soltou a tecla ${event.key}`);
+});
+
+document.addEventListener("keydown", (event) => {
+  console.log(`apertou a tecla ${event.key}`);
+});
